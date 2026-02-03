ca5e64aa87fcd4184fd3a39a891ae989f03b8db4
Brian Henning <henninb@msn.com>
2026-02-03 05:24:35 -0600

    wip

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo dotnet new console -lang F#
-echo dos2unix "*.fs"
-echo dotnet add package Microsoft.EntityFrameworkCore.Sqlite
-echo dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+#echo dotnet new console -lang F#
+#echo dos2unix "*.fs"
+#echo dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+#echo dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
 
 #echo dotnet ef dbcontext scaffold "Host=localhost;Database=finance_db;Username=henninb;Password=monday1" Npgsql.EntityFrameworkCore.PostgreSQL
 
